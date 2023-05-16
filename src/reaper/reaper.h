a63dbe057eb3730b5f70b0886271327b453a7764
HeziCyan <hezicyan@gmail.com>
2023-05-17 01:07:21 +0800

    style: Beautify module reaper

--- src/reaper/reaper.h
+++ src/reaper/reaper.h
@@ -3,14 +3,23 @@
 
 #include "../game/game.h"
 
-namespace ai {
+namespace reaper {
 
-class Ai {
-  public:
-   void GetPossibility(const game::Game& A);
+class Reaper {
+ private:
+  void Dfs(int x, int y);
 
+  const game::Game* game_;
+  int n_, m_, tot_situations_;
+  std::vector<std::vector<int>> cnt_, tot_;
+  std::vector<std::vector<bool>> vis_;
+  std::vector<game::Coord> mines_;
+  std::vector<game::Coord> blocks_;
+
+ public:
+  std::vector<std::vector<double>> GetPossibility(const game::Game* game);
 };
 
-}
+}  // namespace reaper
 
 #endif
