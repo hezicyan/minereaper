8f07fde519f76bb1f9ce8980dab0a4df62dd9f32
WKAHPM <978088232@qq.com>
2023-05-17 12:50:53 +0800

    imfool

--- src/reaper/reaper.cpp
+++ src/reaper/reaper.cpp
@@ -14,12 +14,11 @@ int Sign(double x) {
   return 0;
 }
 
-Reaper::Reaper(game::Game* game) : game_(game) {}
 
 Reaper::~Reaper() {}
 
-bool Reaper::IsValidCoord(const game::Coord& pos) {
-  return pos.x >= 0 && pos.x < n_ && pos.y >= 0 && pos.y < n_;
+bool Reaper::IsValidCoord(const game::Coord& pos) const {
+  return pos.x >= 0 && pos.x < n_ && pos.y >= 0 && pos.y < m_;
 }
 int blocksize;
 void Reaper::GetBlock(int x, int y) {
@@ -42,6 +41,23 @@ void Reaper::GetBlock(int x, int y) {
 int sum;
 void Reaper::Dfs(int t) {
   if (t >= borders_.size()) {
+    // std::cout << "t:" << t << " sum: " << sum << std::endl;
+    // int tmp = 0;
+    // for (int u : mines_) {
+    //   if (borders_[u].x + 1 == 3 and borders_[u].y + 1 == 1) {
+    //     tmp++;
+    //   }
+    //   if (borders_[u].x + 1 == 6 and borders_[u].y + 1 == 4) {
+    //     tmp++;
+    //   }
+    // }
+    // if (tmp == 2) {
+    //   std::cout << "t:" << t << " sum: " << sum << std::endl;
+    //   for (int u : mines_) {
+    //     std::cout << borders_[u].x + 1 << " " << borders_[u].y + 1 << std::endl;
+    //   }
+    // }
+
     if (sum != blocksize) return;
     for (int u : mines_) {
       tot_[borders_[u].x][borders_[u].y]++;
@@ -51,7 +67,6 @@ void Reaper::Dfs(int t) {
   }
   mines_.push_back(t);
   int x = borders_[t].x, y = borders_[t].y;
-
   bool valid = true;
   for (auto p : game::kAdjDirs) {
     int xx = x + p.first;
@@ -60,6 +75,7 @@ void Reaper::Dfs(int t) {
     if (IsValidCoord(pos)) {
       auto now = game_->GetCell(pos);
       if (now.revealed and now.number != 9) {
+        // std::cout << xx << " " << yy << std::endl;
         cnt_[xx][yy]++;
         if (cnt_[xx][yy] == now.number) sum++;
         if (cnt_[xx][yy] > now.number) valid = false;
@@ -75,6 +91,7 @@ void Reaper::Dfs(int t) {
     if (IsValidCoord(pos)) {
       auto now = game_->GetCell(pos);
       if (now.revealed and now.number != 9) {
+        // std::cout << xx << " " << yy << std::endl;
         if (cnt_[xx][yy] == now.number) sum--;
         cnt_[xx][yy]--;
       }
@@ -89,7 +106,7 @@ std::vector<std::vector<double>> Reaper::GetPossibility() {
   mines_.clear();
   vis_ = std::vector<std::vector<bool>>(n_, std::vector<bool>(m_, false));
   // std::cerr << "--------------------------------" << std::endl;
-  std::vector<std::vector<double>> res(n_, std::vector<double>(m_, 0));
+  std::vector<std::vector<double>> res(n_, std::vector<double>(m_, 0.0));
   for (int i = 0; i < n_; i++) {
     for (int j = 0; j < m_; j++) {
       if (vis_[i][j]) continue;
@@ -105,11 +122,10 @@ std::vector<std::vector<double>> Reaper::GetPossibility() {
         tot_ = std::vector<std::vector<int>>(n_, std::vector<int>(m_, 0));
         GetBlock(i, j);
         Dfs(0);
-        std::cout << borders_.size() << std::endl;
-        std::cout << tot_situations_ << std::endl;
-        std::cout << blocksize << std::endl;
+        // std::cout << borders_.size() << std::endl;
+        // std::cout << tot_situations_ << std::endl;
+        // std::cout << blocksize << std::endl;
         for (auto u : borders_) {
-          std::cout << tot_[u.x][u.y] << std::endl;
           res[u.x][u.y] = (double)tot_[u.x][u.y] / tot_situations_;
         }
       }
